807790dca4bac1c23a2ff69cb6fddb60c998a0d0
Matt Yeend <mattyeend1@hotmail.co.uk>
2021-03-29 16:06:27 +0100

    layout image change

--- resources/views/layouts/layout.blade.php
+++ resources/views/layouts/layout.blade.php
@@ -10,7 +10,7 @@
         <link href="https://fonts.googleapis.com/css?family=Nunito:200,600" rel="stylesheet">
 
         <!-- Styles -->
-        <link rel="icon" href="{{ URL::asset('../img/pokedex-app-icon.png') }}" type="image/x-icon"/>
+        <link rel="icon" href="{{ asset('img/pokedex-app-icon.png') }}" type="image/x-icon"/>
         <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
     </head>
     <body>
