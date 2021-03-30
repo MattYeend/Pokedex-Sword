44c4bc9b11f9dd4fba84acf08b980a464f1e2bbe
Matt Yeend <mattyeend1@hotmail.co.uk>
2021-03-30 10:35:16 +0100

    Pokedex show index page change

--- resources/views/pokedex/show.blade.php
+++ resources/views/pokedex/show.blade.php
@@ -2,7 +2,7 @@
 
 @section('content')
 <!-- Styles -->
-<link rel="stylesheet" href="../css/pokedex_show.css">
+<link rel="stylesheet" href="{{ asset('css/pokedex_index.css') }}">
 
 <div class="wrapper">
     <div class="row">
