c035c7157eb873a305b5b169415bfca89241143a
Matt Yeend <mattyeend1@hotmail.co.uk>
2021-03-31 14:37:17 +0100

    change css sheet in show page

--- resources/views/pokedex/show.blade.php
+++ resources/views/pokedex/show.blade.php
@@ -2,7 +2,7 @@
 
 @section('content')
 <!-- Styles -->
-<link rel="stylesheet" href="{{ asset('css/pokedex_index.css') }}">
+<link rel="stylesheet" href="{{ asset('css/pokedex_show.css') }}">
 
 <div class="wrapper">
     <div class="row">
